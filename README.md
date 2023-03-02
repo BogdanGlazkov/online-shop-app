62fa557902ff8968e9bef5c63feba5c9c260cd81
BogdanGlazkov <bglazkov@i.ua>
2023-03-03 00:10:47 +0200

    Add data

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# online-shop-app
+# Website built with Node.js, Auth0, React, React Router, React hooks, Redux, Redux toolkit, AOS.js animations, tsParticles, CSS3, JSX.
+
+## In the project directory, you can run:
+
+## npm i
+## npm start
---
+++ src/data/dataGadgets.js
@@ -0,0 +1,115 @@
+const dataGadgets = [
+  {
+    id: 1,
+    price: 250,
+    img: "phone1",
+    name: "Samsung Galaxy M33",
+    category: "PHONES",
+  },
+
+  {
+    id: 2,
+    price: 500,
+    img: "phone2",
+    name: "Apple iPhone 13",
+    category: "PHONES",
+  },
+
+  {
+    id: 3,
+    price: 300,
+    img: "phone3",
+    name: "Infinix Note 12",
+    category: "PHONES",
+  },
+
+  {
+    id: 4,
+    price: 400,
+    img: "phone4",
+    name: "Nokia G10",
+    category: "PHONES",
+  },
+
+  {
+    id: 5,
+    price: 250,
+    img: "phone5",
+    name: "Xiaomi Redmi Note 11",
+    category: "PHONES",
+  },
+
+  {
+    id: 6,
+    price: 200,
+    img: "phone6",
+    name: "Poco M5",
+    category: "PHONES",
+  },
+
+  {
+    id: 7,
+    price: 250,
+    img: "laptop1",
+    name: "Acer Aspire 7",
+    category: "LAPTOPS",
+  },
+
+  {
+    id: 8,
+    price: 200,
+    img: "laptop2",
+    name: "Asus TUF A15",
+    category: "LAPTOPS",
+  },
+
+  {
+    id: 9,
+    price: 200,
+    img: "TV1",
+    name: "Samsung Odyssey G7",
+    category: "TV",
+  },
+
+  {
+    id: 10,
+    price: 150,
+    img: "TV2",
+    name: "AOC 24G",
+    category: "TV",
+  },
+
+  {
+    id: 11,
+    price: 150,
+    img: "TV3",
+    name: "Philips 24M",
+    category: "TV",
+  },
+
+  {
+    id: 12,
+    price: 100,
+    img: "tablet1",
+    name: "Lenovo Tab M10",
+    category: "TABLETS",
+  },
+
+  {
+    id: 13,
+    price: 130,
+    img: "tablet2",
+    name: "Apple iPad 2021",
+    category: "TABLETS",
+  },
+
+  {
+    id: 14,
+    price: 250,
+    img: "tablet3",
+    name: "Samsung Galaxy Tab A8",
+    category: "TABLETS",
+  },
+];
+
+export default dataGadgets;
